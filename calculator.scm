a5e8fe59625bebe2259d8144f45eac0073eee846
Nehir Tıraş <149995206+nehirtiras@users.noreply.github.com>
2025-03-26 20:52:06 +0300

    calculator.scm

--- calculator.scm
+++ calculator.scm
@@ -1,26 +1,49 @@
-; Scheme Hesap Makinesi
-(define (display-help)
-  (display "\nScheme Calculator - Kullanım Kılavuzu\n")
-  (display "----------------------------------\n")
-  (display "Temel İşlemler: (+ 5 3), (* 10 2), (/ 20 4)\n")
-  (display "Değişken Atama: (define x (+ 5 3))\n")
-  (display "Çıkış: (exit)\n\n"))
+#!/usr/bin/env scheme-script
+(import (scheme base)
+        (scheme write)
+        (scheme read)
+        (scheme process-context))
 
-(define (start-calculator)
+(define *variables* (make-eq-hashtable))
+
+(define (display-help)
+  (display "\n***Scheme Calculator***")
+
+(define (eval-expr expr)
+  (cond
+    ((number? expr) expr)
+    ((symbol? expr) 
+     (let ((val (hashtable-ref *variables* expr #f)))
+       (if val val (error "Tanımsız değişken:" expr))))
+    ((pair? expr)
+     (apply (eval (car expr) (interaction-environment))
+            (map eval-expr (cdr expr))))
+    (else (error "Geçersiz ifade:" expr))))
+
+(define (handle-input input)
+  (guard (ex ((error-object? ex)
+              (display "Hata: ")
+              (display (error-object-message ex))
+              (newline)))
+    (let ((result (eval-expr input)))
+      (when (not (eq? input 'exit))
+        (display "= ")
+        (write result)
+        (newline)
+      result)))
+
+(define (main)
+  (display "Scheme Hesap Makinesi v2.0\n")
   (display-help)
   (let loop ()
     (display "> ")
     (let ((input (read)))
-    (cond
-      ((eq? input 'exit) (display "Çıkılıyor...\n"))
-      ((eq? input 'help) (display-help) (loop))
-      (else
-       (let ((result (eval input (interaction-environment))))
-         (display "= ")
-         (display result)
-         (newline)
+      (cond
+        ((eof-object? input) (display "exit\n"))
+        ((eq? input 'exit) (display "exit\n"))
+        ((eq? input 'help) (display-help) (loop))
+        (else
+         (handle-input input)
          (loop))))))
 
-; Ana programı başlat
-(display "Scheme Hesap Makinesi Başlatılıyor...\n")
-(start-calculator)
+(main)
